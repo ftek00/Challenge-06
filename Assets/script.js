3c5228225fbfc04dacb14ff59c91c10ca600a93b
Feleg Teklezghi <felegteklezghi@gmail.com>
2024-07-09 22:38:27 -0400

    created event listener, linked api, and fetched data

---
+++ Assets/script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const cityInput = document.getElementById("cityInput");
+  const cityButton = document.getElementById("cityButton");
+  const searchHistoryContainer = document.getElementById(
+    "searchHistoryContainer"
+  );
+  const currentWeather = document.getElementById("currentWeather");
+  const futureWeather = document.getElementById("futureWeather");
+
+  function getWeather(city) {
+    const apiKey = "f209a958e2983936d2cd31398fee2ef1";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const weatherData = {
+          city: data.name,
+          date: new Date(data.dt * 1000).toLocaleDateString(),
+          temperature: Math.round(data.main.temp - 273.15),
+          humidity: data.main.humidity,
+          windSpeed: data.wind.speed,
+          icon: data.weather[0].icon,
+        };
+        displayCurrentWeather(weatherData);
+
+        const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+        return fetch(forecastApiUrl);
+      })
+      .then((forecastResponse) => forecastResponse.json())
+      .then((forecastData) => {
+        const fiveDayForecast = forecastData.list.filter(
+          (item, index) => index % 8 === 0
+        );
+        displayFutureWeather(fiveDayForecast);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+});
